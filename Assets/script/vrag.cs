d6a5aaebe514b72314e57a5e75058f1f151b48a8
yuraruy <yuraruy2705@mail.ru>
2018-07-05 19:44:04 +0900

    patch 0.04A

--- Assets/script/vrag.cs
+++ Assets/script/vrag.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class vrag : MonoBehaviour {
-    public GameObject player;
     public float speed = 3.0f;
-    public float objactr = 5f;
+    public GameObject player;
+    
 
     private void OnTriggerEnter2D(Collider2D col)
-    {        
-            Destroy(player);
+    {
+        if (col.gameObject.tag=="player")
+                Destroy(player);
+        if (col.gameObject.tag == "Stena")
+                speed = -speed;
+        
     }
     void Update()
     {
         transform.Translate(speed * Time.deltaTime,0,0);
-
-
     }
 
 }
